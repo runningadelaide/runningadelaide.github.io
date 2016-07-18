ac38be0f0be8f84871bc75e6db8312aac6415dea
Eric Lam <erickslam@gmail.com>
2016-07-19 08:03:07 +0930

    added test google map link

--- js/index/gsscall2.js
+++ js/index/gsscall2.js
@@ -8,8 +8,8 @@ var gss_wedtraining_div_id = "wedtraining";
 var gss_sattraining_div_id = "sattraining";
 var gss_race_div_id = "racesdiv";
 
-//google.load('visualization', '1', {'packages':['corechart']});
-//google.setOnLoadCallback(getTrainingPlan);
+google.load('visualization', '1', {'packages':['corechart']});
+google.setOnLoadCallback(getTrainingPlan);
 
 var weekday = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
 var month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
@@ -20,10 +20,6 @@ var month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","
 // the following is the ARC SS
 var ssKey = "1SQq7DSinO7do-sdqUjZZb0sV437SHRMICMlUQlFFy-g";
 var gurl = "https://docs.google.com/spreadsheets/d/" + ssKey + "/gviz/tq";
-
-var gurl2 = "http://spreadsheets.google.com/a/google.com/tq?key=1SQq7DSinO7do-sdqUjZZb0sV437SHRMICMlUQlFFy-g&tq=select%20*%20limit%2010&tqx=responseHandler:getTrainingPlan2";
-
-
 
 function getDateTimeForQuery() {
 	var d = new Date();
@@ -74,57 +70,6 @@ function getTrainingPlan() {
 	query.send(handleQueryResponse);
 }
 
-function getTrainingPlan2(data) {
-	console.log(data);
-	parse(data);
-	//console.log(parse(data));
-}
-
-function parse(data) {
-
-	var wedString = [];
-	var satString = [];
-	var raceString = [];
-
-    var table = data.table;
-    for (var i = 0; i < table.rows.length; i++){
-    
-    	var date = ( new Function( 'return ' + getStringValue(table,i,0) ) )(); 
-    	var day = getStringValue(table,i,1);
-    	var description = getStringValue(table,i,2);
-    	var location = getStringValue(table,i,3);
-    	var effort = getStringValue(table,i,4);
-    	var race = getStringValue(table,i,5) == 'Race';
-    	
-    	
-		if(race){
-			var ddString = getDateDiff(new Date(), new Date(date));
-			raceString.push(generateDisplayVal(date,day,description,location,effort,race,ddString));
-		} else if(day == 'Wednesday'){
-			wedString.push(generateDisplayVal(date,day,description,location,effort,race));
-		} else if (day == 'Saturday') {
-			satString.push(generateDisplayVal(date,day,description,location,effort,race));
-		}
-    
-    }
-    
-    populatePTag (wedString[0],satString[0],raceString[0]);
-	populateExtraTS (wedString,satString, raceString);
-}
-
-function getStringValue(table, row, col) {
-	
-	if (table.rows[row] && 
-			table.rows[row].c && 
-			table.rows[row].c[col] &&
-			table.rows[row].c[col].v ) {
-		return table.rows[row].c[col].v;
-	} else {
-		return "";
-	}	
-
-}
-
 function handleQueryResponse(response) {
 	if (response.isError()) {
 		console.error('Problem getting training plan ' + response.getMessage() + ' ' + response.getDetailedMessage());
@@ -133,31 +78,6 @@ function handleQueryResponse(response) {
 		var data = response.getDataTable();
 		populateData(data);
 	}
-}
-
-function generateDisplay(date,day,type,location,effort,race) {
-	var rowCount = dataTable.getNumberOfRows();
-	
-	var wedString = [];
-	var satString = [];
-	var raceString = [];
-
-	for (var i = 0; i < rowCount; i++) {
-		var dateVal = dataTable.getValue(i,0);
-		// not all races are on sundays
-		if(dataTable.getValue(i,5) == 'Race'){
-			var ddString = getDateDiff(new Date(), dataTable.getValue(i,0));
-			raceString.push(formatTrainString(dataTable,i,ddString));
-		} else if(dataTable.getValue(i,1) == 'Wednesday'){
-			wedString.push(formatTrainString(dataTable,i));
-		} else if (dataTable.getValue(i,1) == 'Saturday') {
-			satString.push(formatTrainString(dataTable,i));
-		}
-	}
-	
-	//console.log(raceString);
-	populatePTag (wedString[0],satString[0],raceString[0]);
-	populateExtraTS (wedString,satString, raceString);
 }
 
 function populateData(dataTable) {
@@ -185,8 +105,7 @@ function populateData(dataTable) {
 	populateExtraTS (wedString,satString, raceString);
 }
 
-function getDateString(dateValIn) {
-	dateVal = new Date(dateValIn);
+function getDateString(dateVal) {
 	if (dateVal) {
 		return weekday[dateVal.getDay()] + " " +
 			month[dateVal.getMonth()] + " " +
@@ -194,29 +113,6 @@ function getDateString(dateValIn) {
 	} else {
 		return "";
 	}
-}
-
-function generateDisplayVal(date,day,description,location,effort,race,dateDiffString) {
-	var dateString = getDateString(date);
-	
-	if (dateDiffString) {
-		dateString = dateString + " " + dateDiffString;
-	}
-
-	if (location == null) {
-		location = "";
-	} else {
-		location = location + ",";
-	}
-	
-	if (effort == null) {
-		effort = "";
-	}
-	
-	return "<b>" + dateString + "</b><br/>" +
-				location + 
-				effort + " <br/>" +
-				description;
 }
 
 function formatTrainString(dataTable, rowNumber, dateDiffString) {
@@ -239,8 +135,12 @@ function formatTrainString(dataTable, rowNumber, dateDiffString) {
 		effort = "";
 	}
 	
+	var urlComp = encodeURIComponent(location + " " + description + " SA Australia");
+	var url = "http://maps.google.com/?q=" + urlComp;
+	
+	
 	return "<b>" + dateString + "</b><br/>" +
-				location + 
+			"<a href='" + url + "'>" + location + "</a>" +
 				effort + " <br/>" +
 				description;
 }
@@ -260,6 +160,7 @@ function formatRaceString(dataTable, rowNumber) {
 	if (effort == null) {
 		effort = "";
 	}
+	
 	
 	return "<b>" + dateString + "</b> " +
 				location + " " +
